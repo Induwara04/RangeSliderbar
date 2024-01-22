c9a2460cbd4856c341f7330913a993266b761657
Induwara <dinduwaragamage@gmail.com>
2024-01-22 14:16:25 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,29 @@ import ReactSlider from 'react-slider';
 
 function App() {
   return (
-    <div className='body'>
     <div className="App">
       <h4>Range Slider</h4>
-      <ReactSlider
-        className="horizontal-slider"
-        thumbClassName="example-thumb"
-        trackClassName="example-track"
-        defaultValue={[0, 100]}
-        ariaLabel={['Lower thumb', 'Upper thumb']}
-        ariaValuetext={state => `Thumb value ${state.valueNow}`}
-        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
-        onChange={(value, index) => console.log(`onChange: ${JSON.stringify({ value, index })}`)}
-        pearling
-        minDistance={10}
-      />
+      
+        <div>
+        <ReactSlider
+          className="horizontal-slider"
+          thumbClassName="example-thumb"
+          trackClassName="example-track"
+          defaultValue={[0, 100]}
+          ariaLabel={['Lower thumb', 'Upper thumb']}
+          ariaValuetext={state => `Thumb value ${state.valueNow}`}
+          renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+          onChange={(value, index) => console.log(`onChange: ${JSON.stringify({ value, index })}`)}
+          pearling
+          minDistance={10}
+        />
+        <div className='valuerange'>
+        <span className='number' id='valuenumber'>50</span>
+      </div>
+      </div>
     </div>
-    </div>
+  
+
   );
 }
 
